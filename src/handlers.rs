93ce89061c615d5536ac2a8a92d1ad39ec6c0272
Alexander Krikun <krikun.contact@ya.ru>
2020-05-18 12:55:09 +0700

    Abstract boilerplate code

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,89 +1,68 @@
-use std::{
-    io,
-    io::Write,
-};
-
 use actix_web::{
-    HttpRequest, HttpResponse,
-    Responder,
+    HttpRequest, HttpResponse
 };
 
 use serde::{Serialize, Deserialize};
 
 use url_query::UrlQuery;
-use super::solar;
+use super::solar::ground_radiation_for_year;
 
 #[derive(Serialize, Deserialize)]
 pub struct YearIrradiance {
     data: Vec<f64>,
 }
 
+fn check_result<T, Err, Pred, Handler>(pattern: Result<T, Err>, predicate: Pred, otherwise: Handler)
+    -> Result<T, String>
+where Pred: Fn(&T) -> bool,
+    Handler: Fn(Result<T, Err>) -> String 
+{
+    match pattern {
+        Ok(x) if predicate(&x) => Ok(x),
+        other=> Err(otherwise(other)),
+    }
+}
+
 // Original comments are marked with OR.CO.
-pub async fn do_arloste(request: HttpRequest) -> impl Responder {
-    let mut log_stream = io::stdout();
-    let fallback_response = HttpResponse::Ok().json(format!("¯\\_(ツ)_/¯")); // TODO: think of another appropriate default fallback response
+pub async fn do_arloste(request: HttpRequest) -> Result<HttpResponse, String> {
+    let query = UrlQuery::from( check_result (
+            request.uri().query().ok_or("Nothing to handle: no query!"),
+            |_| true, |other| String::from(other.unwrap_err()) // since predicate is always true we can be sure 'other' is Err
+        )?
+    );
 
-    let query = request.uri().query();
-    let query = match query {
-        Some(raw_values) => UrlQuery::from(raw_values),
-        None => {
-            log_stream.write("Nothing to handle: no request query!\n".as_bytes()).unwrap();
-            log_stream.flush().unwrap();
-            return fallback_response;
-        }
-    };
-
-    let latitude = match query.get_of_type::<f64>("lat") {
-        Ok(x) if -90.0 <= x && x <= 90.0 => x.to_radians(),
-        res => {
-            let output = match res {
-                Ok(x) => format!("Latitude {} doesn't fitn\n", x),
+    let latitude = check_result(query.get_of_type::<f64>("lat"), 
+        |&x| -90.0 <= x && x <= 90.0,
+        |other| {
+            match other {
+                Ok(x) => format!("Latitude {} doesn't fit\n", x),
                 Err(msg) => format!("{}\n", msg),
-            };
-            log_stream.write(output.as_bytes()).unwrap();
-            log_stream.flush().unwrap();
-            return fallback_response;
-        },
-    };
+            }
+        })?;
 
-    let t_min: f64 = match query.get_of_type::<f64>("tmin") {
-        Ok(x) if -273.15 <= x && x <= 100.0 => x,
-        res => {
-            let output = match res {
-                Ok(x) => format!("Minimal temperature {} doesn't fit\n", x),
-                Err(msg) => format!("{}\n", msg),
-            };
-            log_stream.write(output.as_bytes()).unwrap();
-            log_stream.flush().unwrap();
-            return fallback_response;
-        },
-    };
+    let t_min = check_result(query.get_of_type::<f64>("tmin"), 
+        |&x| -273.15 <= x && x <= 100.0,
+        |other| match other {
+            Ok(x) => format!("Minimal temperature {} doesn't fit\n", x),
+            Err(msg) => format!("{}\n", msg),
+        })?;
 
-    let t_max: f64 = match query.get_of_type::<f64>("tmax") {
-        Ok(x) if t_min < x && x <= 100.0 => x,
-        res => {
-            let output = match res {
-                Ok(x) => format!("Maximal temperature {} doesn't fit (minimal is {})\n", x, t_min),
-                Err(msg) => format!("{}\n", msg),
-            };
-            log_stream.write(output.as_bytes()).unwrap();
-            log_stream.flush().unwrap();
-            return fallback_response;
-        },
-    };
+    let t_max = check_result(query.get_of_type::<f64>("tmax"),
+        |&x| t_min < x && x <= 100.0,
+        |other| match other {
+            Ok(x) => format!("Maximal temperature {} doesn't fit\n", x),
+            Err(msg) => format!("{}\n", msg),
+        })?;
     
     // TODO: think on leap year detection
-    let daily_solar_rad = solar::ground_radiation_for_year(latitude, t_min, t_max);
+    let daily_solar_rad = ground_radiation_for_year(latitude, t_min, t_max);
 
     let year_data = YearIrradiance{data:daily_solar_rad};
 
-    HttpResponse::Ok().json(format!("{}",
-        match serde_json::to_string(&year_data) {
+    Ok(HttpResponse::Ok()
+        .json(format!("{}", match serde_json::to_string(&year_data) {
             Ok(x) => x,
-            Err(_) => {
-                log_stream.write("Couldn't write json!".as_bytes()).unwrap();
-                return fallback_response;
-            },
-        }
-    ))
+            Err(_) => Err("Couldn't write json!")?
+        }))
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
+#![feature(async_closure)]
+
 use std::{
     net,
-    io,
+    io, io::Write,
 };
 
 use actix_web::{
     web,
     App, 
     HttpServer,
+    HttpResponse,
 };
 
 use simulation_server::{
-    handlers,
+    handlers::do_arloste,
     config::ServerConfig,
 };
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
     let config: ServerConfig = argh::from_env();
 
     let ip: net::IpAddr = config.address.parse()
@@ -27,7 +30,17 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .route("/sim/arloste", web::get().to(handlers::do_arloste))
+            .route("/sim/arloste", web::get().to(async move |r| {
+
+                let mut log_stream = io::stdout(); // TODO: invent how to bring here mutable log_stream and leave closure just Fn and not FnMut or FnOnce
+
+                do_arloste(r).await.unwrap_or_else(|msg| {
+                    log_stream.write(msg.as_bytes()).unwrap();
+                    log_stream.flush().unwrap();
+                    HttpResponse::Ok().json(format!("¯\\_(ツ)_/¯"))
+                })
+
+            }))
     })
     .bind(socket)?
     .run()
